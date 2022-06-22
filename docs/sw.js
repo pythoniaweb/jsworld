5fcc79f029d3f8c77a35d9f9ea2461c51012e025
plurryt <70047632+pythoniaweb@users.noreply.github.com>
2022-06-22 18:04:50 +0300

    Update sw.js
    Signed-off-by: plurryt <70047632+pythoniaweb@users.noreply.github.com>

--- docs/sw.js
+++ docs/sw.js
@@ -13,10 +13,7 @@ const HOSTNAME_WHITELIST = [
   'fonts.googleapis.com',
   'cdn.jsdelivr.net',
   'unpkg.com',
-  'zink.tips',
-  'world.js.cool',
-  'cdn.embedly.com',
-  'github.com'
+  'zink.tips','cdn.embedly.com','github.com'
 ]
 
 // The Util Function to hack URLs of intercepted requests
