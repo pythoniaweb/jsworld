289933d8a377ff7bd818173bc38885886104e12e
plurryt <70047632+pythoniaweb@users.noreply.github.com>
2022-06-22 13:01:50 +0200

    Update https.js
    Signed-off-by: plurryt <70047632+pythoniaweb@users.noreply.github.com>

--- docs/assets/js/https.js
+++ docs/assets/js/https.js
@@ -1,6 +1,3 @@
-// JS World Technology's
-
 if (location.protocol !== 'https:') {
-    console.error("http detected! Making https:")
     location.replace(`https:${location.href.substring(location.protocol.length)}`);
 } 
